0111eccf60e383db8990871c3e9264641c716821
alex_pyslar <alex_pyslar@DESKTOP-PCM1GDQ>
2025-08-12 17:47:26 +0300

    Endpoint для поиска продуктов по id категориям

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -62,8 +62,9 @@ func main() {
 	public.HandleFunc("/auth/register", authHandler.Register).Methods("POST")
 	public.HandleFunc("/auth/login", authHandler.Login).Methods("POST")
 	public.HandleFunc("/products", productHandler.ListProducts).Methods("GET") // Список товаров доступен всем
-	public.HandleFunc("/products/{id}", productHandler.GetProduct).Methods("GET")
-	public.HandleFunc("/categories", categoryHandler.ListCategories).Methods("GET") // Список категорий доступен всем
+	public.HandleFunc("/products", productHandler.ListProducts).Methods("GET")
+	public.HandleFunc("/products/category/{category_id}", productHandler.ListProductsByCategory).Methods("GET")
+	public.HandleFunc("/categories", categoryHandler.ListCategories).Methods("GET")
 	public.HandleFunc("/categories/{id}", categoryHandler.GetCategory).Methods("GET")
 
 	// --- Protected маршруты (доступны авторизованным пользователям) ---
